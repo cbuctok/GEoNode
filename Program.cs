0a01e2eaff453c988f813e051347aaff5c70be0f
Greg <cbuctok@gmail.com>
2019-06-19 13:34:17 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GeoNodes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            alglib.read_csv("./postneStevilke.csv", ',', 0x1, out var matrix);
+            Console.WriteLine("Sup");
+        }
+    }
+}
